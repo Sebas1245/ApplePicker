b3aa255fa0d0fe2174dccbf28847c91bd1e3ff73
Sebastián Saldaña <A01570274@itesm.mx>
2020-11-02 16:20:18 -0600

    adds AppleTree as prefab and makes initial changes to apple tree script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    // Prefab for instatiating objects 
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves in meters/second
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change direction
+    public float chanceToChangeDirection = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Dropping Apples every second
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Basic movement
+        // Changing direction
     }
 }
