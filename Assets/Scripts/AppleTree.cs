ecaac02ce61d712c1328989ed86f4faf6e927b4e
Sebastián Saldaña <A01570274@itesm.mx>
2020-11-02 16:39:21 -0600

    Modifies scripts for apple tree

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -23,12 +23,33 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         // Dropping Apples every second
+        InvokeRepeating( "DropApple", 2f, secondsBetweenAppleDrops );
     }
 
     // Update is called once per frame
     void Update()
     {
         // Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed*Time.deltaTime;
+        transform.position = pos;
         // Changing direction
+        if ( pos.x < -leftAndRightEdge ) {
+            speed = Mathf.Abs(speed);  // Move right
+        } else if ( pos.x > leftAndRightEdge ) {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+        
+    }
+
+    void FixedUpdate() {
+        // Change direction randomly 
+        if ( Random.value < chanceToChangeDirection ) {
+            speed *= -1; 
+        }    
+    }
+    void DropApple() {
+        GameObject apple = Instantiate( applePrefab ) as GameObject;
+        apple.transform.position = transform.position;
     }
 }
